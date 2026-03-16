64f5276fa3b9ab0860d1a5e81d296d06350da878
Tatiana <tatianabarrera444@gmail.com>
2026-03-16 12:04:22 -0600

    Implementando AuthContext

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,1 +1,24 @@
+"use client";
+import { createContext, useState, useEffect } from "react";
 
+export const authContext = createContext();
+export function AuthProvider({ children }) {
+  const[user, setUser] = useState(null);
+
+  useEffect(() => {
+    const session = localStorage.getItem("session");
+
+    if (session) {
+      setUser(JSON.parse(session));
+    }
+  }, []);
+  const login = (userData) => {
+    localStorage.setItem("session", JSON.stringify(userData));
+    setUser(userData);
+  };
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
