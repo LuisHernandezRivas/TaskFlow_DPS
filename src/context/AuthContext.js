4973c858864033cc37f9ae6c6b7da73ca1616103
Tatiana <tatianabarrera444@gmail.com>
2026-03-18 23:17:13 -0600

    Fix formatting of user state declaration

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -2,24 +2,27 @@
 import { createContext, useState, useEffect } from "react";
 
 export const AuthContext = createContext();
+
 export function AuthProvider({ children }) {
-  const[user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     const session = localStorage.getItem("session");
-
     if (session) {
       setUser(JSON.parse(session));
     }
   }, []);
+
   const login = (userData) => {
     localStorage.setItem("session", JSON.stringify(userData));
     setUser(userData);
   };
+
   const logout = () => {
     localStorage.removeItem("session");
     setUser(null);
   };
+
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
       {children}
